a454a8b35149f7ef99be7c9df5dfa13a9a9d707b
Lee Keitel <lee@keitel.xyz>
2017-08-24 20:40:27 -0500

    Use base codec

--- json.go
+++ json.go
@@ -2,7 +2,6 @@ package codecs
 
 import (
 	"encoding/json"
-	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
 	"github.com/spartanlogs/spartan/event"
@@ -10,7 +9,9 @@ import (
 )
 
 // The JSONCodec encodes/decodes an event as JSON.
-type JSONCodec struct{}
+type JSONCodec struct {
+	codecs.BaseCodec
+}
 
 func init() {
 	codecs.Register("json", newJSONCodec)
@@ -26,14 +27,3 @@ func (c *JSONCodec) Encode(e *event.Event) []byte {
 	j, _ := json.Marshal(data)
 	return j
 }
-
-// EncodeWriter reads events from in and writes them to w
-func (c *JSONCodec) EncodeWriter(w io.Writer, in <-chan *event.Event) {}
-
-// Decode byte slice into an Event. CURRENTLY NOT IMPLEMENTED.
-func (c *JSONCodec) Decode(data []byte) (*event.Event, error) {
-	return nil, nil
-}
-
-// DecodeReader reads from r and creates an event sent to out
-func (c *JSONCodec) DecodeReader(r io.Reader, out chan<- *event.Event) {}
